cd7279b3b888c33c6d2dea2c97d234720871cf37
tyanas <tyanas.go@gmail.com>
2017-04-24 11:52:23 +0300

    initial commit

---
+++ src/components/Details1.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="details">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'details1',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.details {
+    background-color: papayawhip;
+}
+</style>
---
+++ src/components/Goals.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="goals">
+        <h2>Цели</h2>
+        <ul>
+            <li>min Vue</li>
+            <li>lean webpack</li>
+            <li>architecture</li>
+            <li>components</li>
+            <li>animation</li>
+            <li>linting</li>
+            <li>fps</li>
+            <li>design</li>
+            <li>tests</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'goals'
+}
+</script>
+
+<style scoped>
+.goals {
+    background-color: lightcyan;
+}
+</style>
+
